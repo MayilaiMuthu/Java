7d118d1dabf1796373c1df2a40a55053ff4ad245
Muthu <104901002+MayilaiMuthu@users.noreply.github.com>
2025-06-24 18:53:25 +0530

    Update LookAndSay.java
    V2 changes

--- NewJava/src/optionalpack/LookAndSay.java
+++ NewJava/src/optionalpack/LookAndSay.java
@@ -10,73 +10,65 @@ public class LookAndSay {
 		List<String> d = new ArrayList<>();
 		a.add("1");
 		for (int i = 0; i < n; i++) {
-			d.clear();
-			a.stream().forEach(b -> {
-				System.out.print(b + " ");
-			});
-			d.addAll(a);
-			a.clear();
+			a.stream().forEach(System.out::print);
 			int count = 1;
 			int row = 0;
 			boolean flag = false;
-			if (n > i + 1) {
-				if (d.size() > 1) {
-					for (int j = 0; j < d.size() - 1; j++) {
-						if (Integer.parseInt(d.get(j + 1)) != Integer.parseInt(d.get(j))) {
-							count = 1;
-							if (!flag) {
-								a.add(row, String.valueOf(count));
-								row++;
-								a.add(row, String.valueOf(d.get(j)));
-								row++;
-								if (count % 2 == 0) {
-									flag = false;
-								} else {
-									flag = true;
-								}
-							}
-							a.add(row, String.valueOf(count));
-							row++;
-							a.add(row, String.valueOf(d.get(j + 1)));
-							row++;
-
-						} else {
-							if (row > 2) {
-								a.remove(row - 1);
-								a.remove(row - 2);
-								if (!flag) {
-									a.remove(row - 3);
-									a.remove(row - 4);
-									row = row - 4;
-								} else {
-									row = row - 2;
-								}
-							} else if (row > 0) {
-								a.remove(row - 1);
-								a.remove(row - 2);
-								row = row - 2;
-							}
-							count++;
+			d.clear();
+			d.addAll(a);
+			if (d.size() > 1) {
+				a.clear();
+				for (int j = 0; j < d.size() - 1; j++) {
+					if (Integer.parseInt(d.get(j)) != Integer.parseInt(d.get(j+1))) {
+						count = 1;
+						if (!flag) {
 							a.add(row, String.valueOf(count));
 							row++;
 							a.add(row, String.valueOf(d.get(j)));
 							row++;
-							flag = true;
-							// row=0;
+							if (count % 2 == 0) {
+								flag = false;
+							} else {
+								flag = true;
+							}
 						}
+						a.add(row, String.valueOf(count));
+						row++;
+						a.add(row, String.valueOf(d.get(j + 1)));
+						row++;
+					} else {
+						if (row > 2) {
+							a.remove(row - 1);
+							a.remove(row - 2);
+							row = row - 2;
+							if (!flag) {
+								a.remove(row - 1);
+								a.remove(row - 2);
+								row = row - 2;
+							}
+						} else if (row > 0) {
+							a.remove(row - 1);
+							a.remove(row - 2);
+							row = row - 2;
+						}
+						count++;
+						a.add(row, String.valueOf(count));
+						row++;
+						a.add(row, String.valueOf(d.get(j)));
+						row++;
+						flag = true;
 					}
-				} else {
-					a.add("1");
-					a.add("1");
 				}
-				System.out.println("");
+			} else {
+				a.add("1");
 			}
+			System.out.println("");
 		}
 	}
 
 	public static void main(String[] args) {
 		LookAndSay t = new LookAndSay();
-		t.lookAndSay(11);
+		t.lookAndSay(7);
 	}
 
 }
